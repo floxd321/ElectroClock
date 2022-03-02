dc345178f42d9fc5fea62f710c896d022a2eb1a8
mnxxx <98615216+floxd321@users.noreply.github.com>
2022-03-02 21:06:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+﻿function getData(){
+
+    var curent_date = new Date();
+    var seconds = String(curent_date.getSeconds());
+    var hours = String(curent_date.getHours());
+    var minutes = String(curent_date.getMinutes());
+    var int_day = String(curent_date.getDay());
+    var week_days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    var day = week_days[int_day];
+
+    if (hours.length < 2){
+        hours + "0" + hours;
+    }
+    if (seconds.length < 2){
+        seconds = "0" + seconds;
+    }
+    if (minutes.length < 2){
+        minutes = "0" + minutes;
+    }
+
+    document.getElementById("clock_time").innerHTML = `${day} ${hours}:${minutes}:${seconds}`;
+
+}
+
+getData();
+setInterval(getData, 200);
+
+
